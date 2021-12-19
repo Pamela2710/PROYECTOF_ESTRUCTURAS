422cb1cb6fb7754f694cc81e29553b3a208422bb
Guille Cisneros Villota <89537340+GuilleCisneros23@users.noreply.github.com>
2021-12-19 16:42:00 -0500

    EJ10

---
+++ Ejercicio10.py
@@ -0,0 +1,48 @@
+class Ejercicio10:
+    
+     def __init__(self,numero,vertice,edge):
+         self.vertice=[vertice[i] for i in range(numero)]
+         self.edge=[edge[i] for i in range (len(edge))]
+         self.numero=numero
+         self.grafico={}
+         self.elemento=[]
+         self.costo=0
+         self.respuesta=[]
+    
+     def Comparador(self,indice):
+         
+        self.elemento.append(indice)
+        
+        for i in self.grafico[indice]:
+            if i not in self.elemento:
+                self.costo += self.Comparador(i)
+                
+        return self.costo
+     
+     def ComponentesConectados(self):
+         valorTemporal=0
+         
+         for llave in self.vertice:
+             self.grafico[llave]=[]
+             
+         for (i0,i1) in self.edge:
+             self.grafico[i0].append(i1)
+             self.grafico[i1].append(i0)
+         self.elementos=set()
+         
+         for i in self.vertice:
+             if i not in self.elemento:
+                 valorTemporal=self.Comparador(i)
+                 self.respuesta.append(valorTemporal)
+                 
+         return len(self.respuesta)
+                 
+     
+#node = [0,1,2,3,4]
+#tople = [[0,1],[1,2],[2,3]]
+
+#a = Ejercicio10(len(node),node,tople)
+#print(a.ComponentesConectados())
+     
+    
+         
